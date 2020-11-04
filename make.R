4461752333ae8d787b2c04208944d48f3a32fef7
unmnn <uli.niemann@ovgu.de>
2020-11-04 13:13:47 +0100

    update readme

--- make.R
+++ make.R
@@ -1,7 +1,8 @@
 source(file.path("data-raw", "uhreg.R"), encoding = 'UTF-8')
 source(file.path("data-raw", "data_dict.R"), encoding = 'UTF-8')
-rmarkdown::render(input = "README.rmd",
-                  output_options = list(html_preview = FALSE))
-
 devtools::document()
 devtools::check()
+devtools::install()
+devtools::reload()
+rmarkdown::render(input = "README.rmd",
+                  output_options = list(html_preview = FALSE))
